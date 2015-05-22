1cb7b2cd2e7d0b9a62f575afbead5933c5fd7fd9
Bar Haim <bha@Bars-MacBook-Pro.local>
2015-05-22 03:15:54 +0300

    Added app folder

---
+++ app/src/main/java/com/app/volunteen/volunteen/ProfileActivity.java
@@ -0,0 +1,118 @@
+package com.app.volunteen.volunteen;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+public class ProfileActivity extends AppCompatActivity {
+
+    TextView tvUsername;
+    TextView tvAge;
+    TextView tvPoints;
+    TextView tvEvents_Opened;
+    TextView tvEvents_Joined;
+    RelativeLayout relativeLayout;
+    ImageView bgImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_activity);
+
+        relativeLayout = (RelativeLayout) findViewById(R.id.profile_cover_image);
+
+        new LoadBackground("http://52.1.16.223/images/cover/cover_bg.png",
+                "cover_bg").execute();
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class LoadBackground extends AsyncTask<String, Void, Drawable> {
+
+        private String imageUrl , imageName;
+
+        public LoadBackground(String url, String file_name) {
+            this.imageUrl = url;
+            this.imageName = file_name;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Drawable doInBackground(String... urls) {
+
+            try {
+                InputStream is = (InputStream) this.fetch(this.imageUrl);
+                Drawable d = Drawable.createFromStream(is, this.imageName);
+                return d;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                return null;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+        private Object fetch(String address) throws MalformedURLException,IOException {
+            URL url = new URL(address);
+            Object content = url.getContent();
+            return content;
+        }
+
+        @SuppressWarnings("deprecation")
+        @Override
+        protected void onPostExecute(Drawable result) {
+            super.onPostExecute(result);
+            relativeLayout.setBackgroundDrawable(result);
+        }
+    }
+
+}
+
+
+
+
